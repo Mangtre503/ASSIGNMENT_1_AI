7b1093e65efbb831ef0766beb9df287ee60140b9
Mangtre503 <tranthingochuyen1772004@gmail.com>
2025-10-29 10:57:48 +0700

    fix path

--- Frontend/game.js
+++ Frontend/game.js
@@ -5,7 +5,9 @@ function showMenu() {
   menu.render(app);
 }
 async function loadLevel(level) {
-  const res = await fetch(`./SokobanMap/mini_cosmos_${level}.txt`);
+  const res = await fetch(
+    `${window.location.origin}/ASSIGNMENT_1_AI/SokobanMap/mini_cosmos_${level}.txt`
+  );
   const mapData = JSON.parse(await res.text());
 
   const game = new GameScreen(mapData, level, () => showMenu());
